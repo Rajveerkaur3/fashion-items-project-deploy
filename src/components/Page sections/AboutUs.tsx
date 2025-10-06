838ec07bcdedccf32ac032aca1fa68f493dbcb0f
Jaskomal Kaur <jaskomalkaur@academic.rrc.ca>
2025-10-05 21:15:13 -0500

    update some files and solves all the errors

--- src/components/Page sections/AboutUs.tsx
+++ src/components/Page sections/AboutUs.tsx
@@ -13,6 +13,7 @@ export function AboutUs() {
       <p>
         At Trand Aura, we’re not just selling clothes – we’re building a
         community of people who love fashion and self-expression.
+        
       </p>
     </section>
   );
