f1895db9c2bbe4f73034351a52ff62bf139ee53b
rajveer <rajveerkaur3@academic.rrc.ca>
2025-10-26 22:01:17 -0500

    chore: add sample data and test files for newsletter and discount offers

---
+++ src/data/newsletterTestData.ts
@@ -0,0 +1,14 @@
+import type { NewsletterSubscriber } from "./NewsletterSubscriber";
+
+export const newsletterTestData: NewsletterSubscriber[] = [
+  { id: 1, email: "user01@gmail.com" },
+  { id: 2, email: "user02@gmail.com" },
+  { id: 3, email: "user03@gmail.com" },
+  { id: 4, email: "user04@gmail.com" },
+  { id: 5, email: "user05@gmail.com" },
+  { id: 6, email: "user06@gmail.com" },
+  { id: 7, email: "user07@gmail.com" },
+  { id: 8, email: "user08@gmail.com" },
+  { id: 9, email: "user09@gmail.com" },
+  { id: 10, email: "user10@gmail.com" }
+];
